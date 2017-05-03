b1dad2703db5f08ca83c8b880b7a88f3f7828b31
CalvinDawgz <calvindawgz1999@gmail.com>
2017-05-03 09:58:18 -0700

    Working on Data class to handle data from game.

--- expert-lamp/src/expert/lamp/ExpertLamp.java
+++ expert-lamp/src/expert/lamp/ExpertLamp.java
@@ -15,7 +15,7 @@ public class ExpertLamp {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		// TODO code application logic here
+		Object[][][][] data = new Object[9][9][9][9];
 	}
 	
 }
