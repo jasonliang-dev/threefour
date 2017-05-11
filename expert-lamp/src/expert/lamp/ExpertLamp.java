eade1dc4917b1c6ede7e0c906953455aa98bbae4
CalvinDawgz <calvindawgz1999@gmail.com>
2017-05-11 09:50:55 -0700

    Still working on data structure

--- expert-lamp/src/expert/lamp/ExpertLamp.java
+++ expert-lamp/src/expert/lamp/ExpertLamp.java
@@ -1,23 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package expert.lamp;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author jason
- */
 public class ExpertLamp {
-
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) {
+    Player curPlayer;
+    
+    public static void main(String[] args) {
+        UsersData allData = new UsersData();
+        String[] names = new String[] {"Bob", "Sam", "Jim", "Sammy"};
+        
+        for (int i = 0; i < 4; i++) {
+            ArrayList temp = new  ArrayList();
             
-	}
+            temp.add(Math.random());
+            temp.add(Math.random());
+            temp.add(Math.random());
+            temp.add(Math.random());
+            
+            allData.addUser(names[i], temp);
+        }
+        
+        Player curPlayer = allData.setCurrentUser("Bob");
+        Player otherPlayer = allData.players.get(1);
+        
+        System.out.println(otherPlayer.getName());
+        System.out.println(curPlayer.getAvg());
+        System.out.println("Cur: "+curPlayer.avg + " Other: "+otherPlayer.avg);
+        System.out.println(curPlayer.compareTo(otherPlayer));
+    }
 	
 }
