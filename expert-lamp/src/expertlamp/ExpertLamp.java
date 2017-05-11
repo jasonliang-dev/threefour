3528703847ebd24134b499760ac99586cdc746d8
548263 <548263@04-315-MJYTPY7.SD41.ADS>
2017-05-11 09:51:11 -0700

    memes

--- expert-lamp/src/expertlamp/ExpertLamp.java
+++ expert-lamp/src/expertlamp/ExpertLamp.java
@@ -2,8 +2,20 @@ package expertlamp;
 
 public class ExpertLamp {
 
-	public static void main(String[] args) {
-            
+	public static void main(String[] args) throws InterruptedException{
+            long p = System.nanoTime();
+            Thread.sleep(1000);
+            long q = System.nanoTime();
+            long T1 = (q-p);
+            System.out.println(T1);
+
+            long a = System.nanoTime();
+            Thread.sleep(999);
+            long b = System.nanoTime();
+            long T2 = (b-a);
+            System.out.println(T2);
+
+            System.out.println((T2-T1)/1000000);
 	}
 	
 }
