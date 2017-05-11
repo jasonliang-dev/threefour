19feb913ec39b7dbbb8bc5853f821ece5e9191be
CalvinDawgz <calvindawgz1999@gmail.com>
2017-05-11 09:51:14 -0700

    Finished most of classes for Player

--- expert-lamp/src/expert/lamp/Player.java
+++ expert-lamp/src/expert/lamp/Player.java
@@ -2,19 +2,27 @@ package expert.lamp;
 
 import java.util.ArrayList;
 
-public class Player {
+public class Player implements Comparable<Player> {
     String name;
-    double avg;
-    ArrayList<Integer> reactionTime;
+    double avg = 0;
+    ArrayList<Double> reactionTimes = new ArrayList();
     
     Player() {
-        
+        name = "PLEASE ENTER A NAME";
+        reactionTimes = new ArrayList();
+    }
+ 
+    Player (String name) {
+        this.name = name;
+        reactionTimes = new ArrayList();
     }
     
-    Player(String name, double avg, ArrayList<Integer> reactionTime) {
-        this.name = name;
-        this.avg = avg;
-        this.reactionTime = reactionTime;
+    Player(String name, ArrayList<Double> reactionTimes) {
+        if (reactionTimes.size() > 0) {
+            this.name = name;
+            this.reactionTimes = reactionTimes;
+            this.findAvg();
+        }
     }
     
     public String getName() {
@@ -29,7 +37,35 @@ public class Player {
         return avg;
     }
     
-    public void  setAvg(double avg) {
-        this.avg = avg;
+    public void findAvg() {
+        int numOfItems = reactionTimes.size();
+        
+        if (numOfItems > 0) {
+            double sum = 0;
+            
+            for (Double i : reactionTimes) {
+                sum += i;
+            }
+            
+            avg = sum / numOfItems;
+        }
     }
+    
+    /**
+     * 
+     * @param otherPlayer This is another user to compare average times to. 
+     * @return Returns "1" if the other player has a slower time. 
+     * Returns "0" if the times are equal. 
+     * Returns "-1" when the other player has a greater time. 
+     */
+    public int compareTo(Player otherPlayer) {
+        if (avg > otherPlayer.avg)
+            return 1;
+        else if (avg < otherPlayer.avg)
+            return -1;
+        else 
+            return 0;
+    }
+
+
 }
